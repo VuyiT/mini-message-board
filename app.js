ff079226613b0b927a418ecf2015430ed90fd818
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-02-25 15:23:03 +0200

    Remove index router

--- app.js
+++ app.js
@@ -8,22 +8,3 @@ app.listen(PORT, (err) => {
     }
     console.log(`listening for requests on port ${PORT}`);
 });
-
-app.get("/", (req, res) => {
-    const messages = [
-        {
-          text: "Hi there!",
-          user: "Amando",
-          added: new Date()
-        },
-        {
-          text: "Hello World!",
-          user: "Charles",
-          added: new Date()
-        }
-    ];
-
-    res.render("index.ejs", {
-        title: "Messages",
-        messages });
-});
