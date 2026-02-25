cec3fac72d9d8338ceeaf76385ecb529e826dffa
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-02-25 20:06:42 +0200

    Move messages outside get method and push new ones

--- routes/Index.js
+++ routes/Index.js
@@ -1,9 +1,7 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
-indexRouter.route("/")
- .get((req, res) => {
-    const messages = [
+const messages = [
         {
           text: "Hi there!",
           user: "Venka",
@@ -11,16 +9,20 @@ indexRouter.route("/")
         },
         {
           text: "Hello World!",
-          user: "Hayena",
+          user: "Soyun",
           added: new Date()
         }
     ];
 
+indexRouter.route("/")
+ .get((req, res) => {
     res.render("index", {
         title: "Messages",
-        messages });
+        messages: messages });
   })
   .post((req, res) => {
-    console.log(req.body);
+    messages.push({ text: req.body.messageText, user: req.body.messageUser, added: new Date()
+    });
+    res.redirect("/");
   });
 module.exports = indexRouter;
