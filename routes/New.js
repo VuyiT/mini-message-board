919f3333666964979169ce5ea31837928c290efc
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-02-25 19:40:23 +0200

    Remove post request chain from  newMessageRouter

--- routes/New.js
+++ routes/New.js
@@ -1,14 +1,10 @@
 const { Router } = require("express");
 const newMessageRouter = Router();
 
-newMessageRouter.route("/")
- .get((req, res) => {
+newMessageRouter.get("/", (req, res) => {
     res.render("form", {
         title: "New Message"
     })
-  })
-  .post((req, res) => {
-    res.send("POST Request Called");
-  });
+});
 
 module.exports = newMessageRouter;
