f28894f5003f6e5e498288976548072dd20dbbb8
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-02-25 19:02:31 +0200

    Chain a post request to newMessageRouter

--- routes/New.js
+++ routes/New.js
@@ -1,10 +1,14 @@
 const { Router } = require("express");
 const newMessageRouter = Router();
 
-newMessageRouter.get("/", (req, res) => {
+newMessageRouter.route("/")
+ .get((req, res) => {
     res.render("form", {
         title: "New Message"
-    });
-});
+    })
+  })
+  .post((req, res) => {
+    res.send("POST Request Called");
+  });
 
 module.exports = newMessageRouter;
