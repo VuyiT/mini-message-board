e0de59c15a6a8a1c52d378179e99b601354161d6
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-02-25 16:01:38 +0200

    Import and use newMessageRouter

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+
 const indexRouter = require("./routes/Index");
+const newMessageRouter = require("./routes/New");
+
 const PORT = 8080;
 app.listen(PORT, (err) => {
     if (err) {
@@ -11,3 +14,4 @@ app.listen(PORT, (err) => {
 });
 
 app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
