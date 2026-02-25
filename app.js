6abbd0bbf8d70fa2bb62b1b14992f709ac769651
Avuyile Tiki <avuyiletiki14@gmail.com>
2026-02-25 10:03:20 +0200

    Set up basic index route and run server

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(`listening for requests on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("I am listening!");
+});
